642b468f20c0802f6172f40fa8f360f3128f5545
Tyreeze <wedding5@msu.edu>
2021-12-02 14:58:21 -0600

    generate full site

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
-const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -129,22 +129,18 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
-
-
-
-
-
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
-
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('./index.html', pageHTML, err => {
- // if (err) throw err;
-
- // console.log('Portfolio complete! Check out index.html to see the output!');
-//});
